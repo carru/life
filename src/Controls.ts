628bc06b17431edfc1459c2a904796f874c8af50
carru <carru400@gmail.com>
2021-05-07 13:50:59 -0700

    speed controls

--- src/Controls.ts
+++ src/Controls.ts
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import { Renderer } from "./Renderer";
-import { SimulationLoop } from "./SimulationLoop";
+import { SimulationLoop, SimulationSpeeds } from "./SimulationLoop";
 
 export class Controls {
     protected renderer: Renderer;
@@ -15,9 +15,18 @@ export class Controls {
         $("#stop-renderer-btn").on("click", () => this.renderer.stop());
         $("#start-simulation-btn").on("click", () => this.simulationLoop.start());
         $("#stop-simulation-btn").on("click", () => this.simulationLoop.stop());
+        $("#simulation-speed-1-btn").on("click", () => this.updateSpeed(SimulationSpeeds.NORMAL));
+        $("#simulation-speed-2-btn").on("click", () => this.updateSpeed(SimulationSpeeds.FAST));
+        $("#simulation-speed-3-btn").on("click", () => this.updateSpeed(SimulationSpeeds.LUDICROUS));
     }
 
     protected toggle() {
         $("#controls").toggle();
+    }
+
+    protected updateSpeed(speed: SimulationSpeeds): void {
+        this.simulationLoop.stop();
+        this.simulationLoop.speed = speed;
+        this.simulationLoop.start();
     }
 }
